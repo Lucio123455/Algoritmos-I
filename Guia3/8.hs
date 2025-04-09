e27f699da58a8b25e406c523a5351c394d50a2a7
Pc del labo <pcdellabo@gmail.com>
2025-04-09 16:29:56 -0300

    Ejercicio 8

--- Guia3/8.hs
+++ Guia3/8.hs
@@ -15,11 +15,22 @@ comparar 45 312 ⇝ -1 porque 45 ≺312 y 4 + 5 > 1 + 2.
 comparar 2312 7 ⇝ 1 porque 2312 ≺7 y 1 + 2 < 0 + 7.
 comparar 45 172 ⇝ 0 porque no vale 45 ≺172 ni tampoco 172 ≺45-}
 
-sumaUltimosDosDigitos :: Rational -> Rational
-sumaUltimosDosDigitos a = ((absoluto a) * (`mod` 10)) + (((absoluto a) / 10) * (`mod` 10))
+comparar :: Integer -> Integer -> Integer
+comparar a b 
+    | sumaUltimosDosDigitos a < sumaUltimosDosDigitos b     = 1 
+    | sumaUltimosDosDigitos a > sumaUltimosDosDigitos b     = (-1)
+    | otherwise                                             = 0
 
+sumaUltimosDosDigitos :: Integer -> Integer
+sumaUltimosDosDigitos a = (digitoUnidades a) + (digitoDecenas a) 
 
-absoluto :: Rational -> Rational
+absoluto :: Integer -> Integer
 absoluto n
     | n >= 0    = n
     | otherwise = -n
+
+digitoUnidades :: Integer -> Integer
+digitoUnidades n = (absoluto n) `mod` 10
+
+digitoDecenas :: Integer -> Integer
+digitoDecenas n = (absoluto((absoluto n) `div` 10)) `mod` 10
